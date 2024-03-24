b09ccc1db870059530efd9398c302b26ba91498d
Destruction <htim44@gmail.com>
2024-03-24 13:59:57 +0300

    init commit

---
+++ src/main/java/com/destructio/scoringbpmnexample/ScoringBpmnExampleApplication.java
@@ -0,0 +1,13 @@
+package com.destructio.scoringbpmnexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoringBpmnExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScoringBpmnExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/destructio/scoringbpmnexample/ScoringBpmnExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.destructio.scoringbpmnexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoringBpmnExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
